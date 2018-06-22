e907d28d5b39171ff08035f7760714c16cee4790
niephox <rezutasos@gmail.com>
2018-06-23 01:21:34 +0300

    route Comments

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var app = express();
 app.set("view engine","ejs");
 
 
+//LOGIN ROUTE
+/**
+ * Create a route to show the login page and act accordingly
+ * */
+
+//REGISTER ROUTE
+/**
+ * Create a route to show the register page and act accordingly
+ * */
 
 app.get("/",function (req,res) {
     res.render("landing.ejs");
