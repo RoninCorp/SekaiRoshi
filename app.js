6a4c7623d2bac69aeacc91af2b2ccf47d1be1355
KostOrv <konst.orfan@gmail.com>
2018-06-23 16:03:53 +0300

    Add login & register. 'SekaiRoshi' button point to landing

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ app.use(express.static("public"));
 /**
  * Create a route to show the login page and act accordingly
  * */
+app.get("/login", function (req,res) {
+   res.render("login");
+});
 
 //REGISTER ROUTE
 /**
  * Create a route to show the register page and act accordingly
  * */
+app.get("/register", function (req,res) {
+    res.render("register");
+});
+
+app.get("/landing", function (req,res) {
+    res.render("landing");
+});
 
 app.get("/",function (req,res) {
     res.render("landing");
